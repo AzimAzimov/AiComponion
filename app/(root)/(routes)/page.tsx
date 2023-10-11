91c13783f093ff3c03ed8fb006cf3a5bb8931c7e
Azim Azimov <mrazim0990@gmail.com>
2023-10-12 01:01:02 +0300

    main

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,11 +1,17 @@
+import Categoryes from '@/components/Categoryes'
 import SearchInput from '@/components/Search'
+import prismadb from '@/lib/prismadb'
 import { UserButton } from '@clerk/nextjs'
-import React from 'react'
+import React, { FC } from 'react'
 
-const RootPage = () => {
+
+const RootPage:FC = async () => {
+  const categoryes = await prismadb.category.findMany()
+
   return (
     <div className='h-full p-4 space-y-2'>
       <SearchInput />
+      <Categoryes data={categoryes} />
     </div>
   )
 }
